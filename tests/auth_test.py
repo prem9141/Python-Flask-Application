2a1f949416ef0b888247dee9f547c334c2addeea
pm42 <pm42@njit.edu>
2022-04-29 15:22:35 -0400

    Added Invalid Password Test Case

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -8,15 +8,15 @@ from werkzeug.security import generate_password_hash
 def test_request_login(client):
     """This makes the valid login page"""
     with client:
+        assert db.session.query(User).count() == 0
         email = 'prem@test.com'
         password = 'prem@1234'
         user = User(email, generate_password_hash(password))
         db.session.add(user)
         db.session.commit()
         assert db.session.query(User).count() == 1
-        client.post('/login', data={"email": email, "password": password})
-        response = client.get('/dashboard')
-        assert response.status_code == 200
+        response = client.post('/login', data={"email": email, "password": password}, follow_redirects=True)
+        assert b"Welcome" in response.data
 
 
 def test_request_login_fail_user(client):
@@ -25,6 +25,21 @@ def test_request_login_fail_user(client):
         assert db.session.query(User).count() == 0
         email = 'prem@test.com'
         password = 'prem@1234'
-        client.post('/login', data={"email": email, "password": password})
-        response = client.get('/dashboard')
-        assert response.status_code == 302
+        response = client.post('/login', data={"email": email, "password": password}, follow_redirects=True)
+        assert b"Invalid username or password" in response.data
+
+
+def test_request_login_fail_user_password(client):
+    """This makes the valid user but invalid password page"""
+    with client:
+        assert db.session.query(User).count() == 0
+        email = 'prem2@test.com'
+        password = 'prem2@1234'
+        user = User(email, generate_password_hash(password))
+        db.session.add(user)
+        db.session.commit()
+        assert db.session.query(User).count() == 1
+        response = client.post('/login', data={"email": email, "password": "prem@1234"}, follow_redirects=True)
+        assert b"Invalid username or password" in response.data
+
+
