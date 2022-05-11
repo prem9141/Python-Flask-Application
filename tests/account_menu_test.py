d118cd99659b05840f42decd8b20735511292bb4
pm42 <pm42@njit.edu>
2022-05-11 17:35:01 -0400

    Added Manage Account Test Case

--- tests/account_menu_test.py
+++ tests/account_menu_test.py
@@ -2,6 +2,7 @@
 
 from app import db
 from app.db.models import User
+from werkzeug.security import check_password_hash
 
 
 def test_request_manage_profile(client, add_user):
@@ -14,6 +15,23 @@ def test_request_manage_profile(client, add_user):
         response = client.post('/profile', data={"about": 'My Profile About Test'}, follow_redirects=True)
         assert b"You Successfully Updated your Profile" in response.data
         assert user.about == 'My Profile About Test'
+
+
+def test_request_manage_account(client, add_user):
+    """This makes the manage account page"""
+    with client:
+        assert db.session.query(User).count() == 1
+        user = User.query.get(1)
+
+        old_password = 'prem@1234'
+        client.post('/login', data={'email': user.email, 'password': old_password})
+        assert check_password_hash(user.password, old_password)
+
+        new_password = 'new@1234'
+        response = client.post('/account', data={'email': user.email, 'password': new_password, 'confirm': new_password}, follow_redirects=True)
+        assert b"You Successfully Updated your Password or Email" in response.data
+        assert check_password_hash(user.password, new_password)
+
 
 
 
